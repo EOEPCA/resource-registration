789b3ace5819b6826efcdd98cc97fd36c9a49d54
Markus Kunze <markus.kunze.fhk@gmail.com>
2024-07-22 09:11:45 +0200

    Reformat base/files.py

--- lib/test/base/test_file.py
+++ lib/test/base/test_file.py
@@ -2,7 +2,8 @@ import os
 import tempfile
 import pytest
 import zipfile
-from terrabyte.ingestion.base.file import zip_directory, delete_file
+from lib.base.file import zip_directory, delete_file
+
 
 @pytest.fixture(autouse=True)
 def temp_directory():
@@ -19,10 +20,11 @@ def temp_directory():
             os.remove(file_path)
     os.rmdir(temp_dir)
 
+
 def test_zip_directory(temp_directory):
     # Create a test file inside the temporary directory
     test_file = os.path.join(temp_directory, "test.txt")
-    with open(test_file, 'w') as f:
+    with open(test_file, "w") as f:
         f.write("This is a test file.")
 
     # Zip the temporary directory
@@ -35,38 +37,42 @@ def test_zip_directory(temp_directory):
     assert os.path.basename(zip_file_path) == os.path.basename(temp_directory) + ".zip"
 
     # Check if the zip file contains the test file
-    with zipfile.ZipFile(zip_file_path, 'r') as zipf:
+    with zipfile.ZipFile(zip_file_path, "r") as zipf:
         assert "test.txt" in zipf.namelist()
+
 
 def test_zip_directory_invalid_source():
     # Test invalid source path
     with pytest.raises(ValueError):
         zip_directory("/invalid/path")
 
+
 def test_zip_directory_existing_destination(temp_directory):
     # Create a test file inside the temporary directory
     test_file = os.path.join(temp_directory, "test.txt")
-    with open(test_file, 'w') as f:
+    with open(test_file, "w") as f:
         f.write("This is a test file.")
 
     # Create a test zip file in the temporary directory
     test_zip_path = os.path.join(temp_directory, "test.zip")
-    with zipfile.ZipFile(test_zip_path, 'w') as zipf:
+    with zipfile.ZipFile(test_zip_path, "w") as zipf:
         zipf.write(os.path.join(temp_directory, "test.txt"), "test.txt")
 
     # Test destination path already exists
     with pytest.raises(ValueError):
         zip_directory(temp_directory, test_zip_path)
 
+
 def test_delete_file():
     temp_dir = tempfile.mkdtemp()
     test_file = os.path.join(temp_dir, "test_iif.xml")
-    with open(test_file, 'w') as f:
+    with open(test_file, "w") as f:
         f.write("<IIF></IIF>")
 
     delete_file(test_file)
     assert not os.path.isfile(test_file)
     os.rmdir(temp_dir)
+
 
 def test_delete_file_not_existing():
     temp_dir = tempfile.mkdtemp()
@@ -74,6 +80,7 @@ def test_delete_file_not_existing():
     with pytest.raises(Exception):
         delete_file(test_file)
     os.rmdir(temp_dir)
+
 
 def test_delete_file_is_dir():
     temp_dir = tempfile.mkdtemp()
