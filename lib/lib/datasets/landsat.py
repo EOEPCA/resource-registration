cf534f9233c33d45608a12fd71ad23459fdefc36
Markus Kunze <markus.kunze.fhk@gmail.com>
2024-07-22 09:43:48 +0200

    Add various datasets functions

---
+++ lib/lib/datasets/landsat.py
@@ -0,0 +1,268 @@
+import os
+import re
+import json
+import glob
+import copy
+import logging
+import pystac
+import fnmatch
+import requests
+from datetime import datetime
+
+from ..resources.stac import (
+    extract_stactools,
+    add_asset_filesize,
+)
+
+# https://www.usgs.gov/faqs/what-naming-convention-landsat-collections-level-1-scenes
+scene_id_pattern = (
+    r"^L"
+    r"(?P<sensor>C|O|T|E|T|M)"
+    r"(?P<satellite>[0-9]{2})_"
+    r"(?P<processingLevel>[0-9A-Z]{4})_"
+    r"(?P<wrsPath>[0-9]{3})"
+    r"(?P<wrsRow>[0-9]{3})_"
+    r"(?P<start>[0-9]{8})_"
+    r"(?P<processingTime>[0-9]{8})_"
+    r"(?P<collectionNumber>[0-9]{2})_"
+    r"(?P<collectionCategory>[A-Z0-9]{2})$"
+)
+
+sensor_name = {
+    "C": "oli-tirs",
+    "O": "oli",
+    # "T": "tirs",
+    "E": "etm",
+    "T": "tm",
+    "M": "mss",
+}
+
+asset_changes = {
+    "LT": { # for Landsat 4 and 5
+        'blue': ['B01', 'Blue Band (B01)'], 
+        'green': ['B02','Green Band (B02)'],
+        'red': ['B03','Red Band (B03)'],
+        'nir08': ['B04','Near Infrared Band 0.8 (B04)'],
+        'swir16': ['B05','Short-wave Infrared Band 1.6 (B05)'],
+        'lwir': ['B06','Surface Temperature Band (B06)'],     
+        'swir22': ['B07','Short-wave Infrared Band 2.2 (B07)'],
+        'atmos_opacity': ['Atmos_Opacity',''],
+        'atran': ['ATRAN',''],
+        'cdist': ['CDIST',''],
+        'drad': ['DRAD',''],
+        'urad': ['URAD',''],
+        'trad': ['TRAD',''],
+        'emis': ['EMIS',''],
+        'emsd': ['EMSD',''],
+        'qa_pixel': ['QA_Pixel',''],
+        'qa_radsat': ['QA_Radsat',''],
+        'qa': ['QA_Temp',''],
+        'cloud_qa': ['QA_Cloud','']
+    },
+    "LE": { # for Landsat 7
+        'blue': ['B01', 'Blue Band (B01)'], 
+        'green': ['B02','Green Band (B02)'],
+        'red': ['B03','Red Band (B03)'],
+        'nir08': ['B04','Near Infrared Band 0.8 (B04)'],
+        'swir16': ['B05','Short-wave Infrared Band 1.6 (B05)'],
+        'lwir': ['B06','Surface Temperature Band (B06)'],        
+        'swir22': ['B07','Short-wave Infrared Band 2.2 (B07)'],
+        'atmos_opacity': ['Atmos_Opacity',''],
+        'atran': ['ATRAN',''],
+        'cdist': ['CDIST',''],
+        'drad': ['DRAD',''],
+        'urad': ['URAD',''],
+        'trad': ['TRAD',''],
+        'emis': ['EMIS',''],
+        'emsd': ['EMSD',''],
+        'qa_pixel': ['QA_Pixel',''],
+        'qa_radsat': ['QA_Radsat',''],
+        'qa': ['QA_Temp',''],
+        'cloud_qa': ['QA_Cloud','']
+    },
+    "LC": { # for Landsat 8 and 9
+        'coastal': ['B01','Coastal/Aerosol Band (B01)'],
+        'blue': ['B02','Blue Band (B02)'], 
+        'green': ['B03','Green Band (B03)'],
+        'red': ['B04','Red Band (B04)'],
+        'nir08': ['B05','Near Infrared Band 0.8 (B05)'],
+        'swir16': ['B06','Short-wave Infrared Band 1.6 (B06)'],
+        'lwir11': ['B10','Surface Temperature Band (B10)'],        
+        'swir22': ['B07','Short-wave Infrared Band 2.2 (B07)'],
+        'atran': ['ATRAN',''],
+        'cdist': ['CDIST',''],
+        'drad': ['DRAD',''],
+        'urad': ['URAD',''],
+        'trad': ['TRAD',''],
+        'emis': ['EMIS',''],
+        'emsd': ['EMSD',''],
+        'qa_pixel': ['QA_Pixel',''],
+        'qa_radsat': ['QA_Radsat',''],
+        'qa': ['QA_Temp',''],
+        'qa_aerosol': ['QA_Aerosol','']
+    }    
+}
+
+folder_structure = "level-{processingLevelNo}/standard/{sensor}/{year}/{wrsPath}/{wrsRow}"
+
+
+def get_scene_id_info(scene_id):
+    match = re.match(re.compile(scene_id_pattern), scene_id)
+    return match.groupdict()
+
+
+def get_scene_id_folder(scene_id, folder_format=None):
+    variables = get_scene_id_info(scene_id)
+    if "start" in variables:
+        date = datetime.strptime(variables["start"], "%Y%m%d")
+        variables["year"] = date.strftime("%Y")
+        variables["month"] = date.strftime("%m")
+        variables["day"] = date.strftime("%d")
+    if "sensor" in variables:
+        variables["sensor"] = sensor_name[variables["sensor"]]
+    if "processingLevel" in variables:
+        variables['processingLevelNo'] = variables['processingLevel'][1]
+
+    if folder_format is None:
+        folder_format = folder_structure
+
+    return folder_format.format(**variables)
+
+def landsat_metadata(scene_path, scene_id, return_pystac=False, add_file_size=False):
+    if scene_path[-1] == "/":
+        scene_path = scene_path[:-1]
+    print("executing landsat_metadata for %s" % scene_path)
+    
+    if not os.path.exists(scene_path):
+        raise Exception("metadata_error: Folder does not exist %s" % (scene_path))
+    
+    stac_function = 'stactools.landsat.stac.create_item'
+    stac_function_options = {'use_usgs_geometry': False}
+    
+    landsat_mtl_xml = glob.glob(os.path.join(scene_path, "*_MTL.xml"))
+    if len(landsat_mtl_xml) == 0:
+        metadata_error = "No *_MTL.xml file available in folder %s" % (scene_path,)
+        raise Exception(
+            "metadata_error: %s" % metadata_error
+        )
+    landsat_mtl_xml = landsat_mtl_xml[0]
+    print("MTL file: %s" % landsat_mtl_xml)
+
+    try:
+        stac_file = os.path.join(scene_path, scene_id + ".STAC.json")
+        stac_item = extract_stactools(landsat_mtl_xml, stac_function, stac_function_options)
+        stac_item.id = scene_id
+        stac_item = modify_landsat_stac(stac_item)
+        if add_file_size:
+            stac_item = add_asset_filesize(stac_item)
+        if return_pystac:
+            return stac_item
+        else:
+            with open(stac_file, 'w') as f:
+                f.write(json.dumps(stac_item.to_dict()))
+            return stac_file
+
+    except Exception as e:
+        metadata_error = "Error during creating metadata for %s: %s" % (
+            scene_path,
+            str(e),
+        )
+        raise Exception(
+            "metadata_error: %s" % metadata_error
+        )
+
+
+def adapt_stac_metadata(scene_path):
+    """
+    Changes hrefs in existing Landsat STAC-Metadata
+    """
+
+    print(f"Adapt STAC-metadata of {scene_path}.")
+    # for all STAC-metadata files do
+    stac_jsons = []
+    for file in os.listdir(scene_path):
+        if file.endswith("stac.json"):
+            stac_jsons.append(file)
+
+    stac_files = []
+    if len(stac_jsons) > 0:
+        try:
+            for file in stac_jsons:
+                # read in stac.json
+                with open(os.path.join(scene_path, file), "r") as stac_file:
+                    data = json.load(stac_file)
+                    if "created" not in data["properties"]:
+                        data["properties"]["created"] = str(datetime.now().strftime("%Y-%m-%dT%H:%M:%S.%fZ"))
+
+                    # adapt all assets
+                    for asset in data["assets"]:
+                        href_old = data["assets"][asset]["href"]
+                        file_name = os.path.basename(href_old)
+                        # href_new = os.path.join(scene_path,file_name) # keep relative paths
+                        href_new = file_name
+                        data["assets"][asset]["href"] = href_new
+                        if "alternate" in data["assets"][asset]:
+                            del data["assets"][asset]["alternate"]
+
+                    # remove index asset
+                    if "index" in data["assets"]:
+                        del data["assets"]["index"]
+
+                    # remove all links
+                    data["links"] = []
+
+                # write .COG .json file
+                with open(os.path.join(scene_path, file), "w") as jsonFile:
+                    json.dump(data, jsonFile, indent=4)
+                stac_files.append(os.path.join(scene_path, file))
+            print(f"STAC-metadata of {scene_path} successfully adapted")
+
+        except Exception as e:
+            print(e)
+            print(f"Failed to adapt STAC-metadata of {scene_path}")
+    else:
+        print(f"{scene_path} does not contain STAC-metadata to adapt.")
+    
+    return stac_files
+
+log = logging.getLogger("Log Info")
+
+def modify_landsat_stac(stac_item: pystac.item.Item):
+    """Modify the Asset-Keys and eo:bands:name for a Landsat L2 STAC-Item.
+    
+    Args:
+        stac_item: The STAC item file to modify. Must be a STACObject.
+        
+        Returns: A pystac.item.Item object with the desired changes."""
+
+    stac_item_dict = copy.deepcopy(stac_item.to_dict(include_self_link=False))
+
+    if stac_item_dict['geometry']['type'] == 'MultiPolygon':
+        try:
+            link = stac_item_dict['links'][1]['href']
+            stac_item_usgs = requests.get(link).json()
+            stac_item_dict['geometry'] = stac_item_usgs['geometry']
+        except Exception as e:
+            pass
+
+    mission = stac_item.id[0:2]   # Get first two characters of Item id (e.g., LC for LC09_L2SR_....)
+    if mission not in asset_changes:
+        raise Exception("Could not find entry for %s in asset_changes configuration" % mission)
+    input_dict = asset_changes[mission]
+    
+    for i, (current_key, target_key) in enumerate(input_dict.items()):
+        log.info(f'Replacing the current Asset-Key {current_key} with the new Asset-Key {target_key[0]}.')
+        try:
+            stac_item_dict["assets"][target_key[0]] = copy.deepcopy(stac_item_dict["assets"].pop(current_key))
+            if "eo:bands" in stac_item_dict["assets"][target_key[0]]:
+                stac_item_dict["assets"][target_key[0]]["eo:bands"][0]["name"] = target_key[0]
+                stac_item_dict["assets"][target_key[0]]["title"] = target_key[1]
+        except:
+            log.info(f'{current_key} is not a Asset in this STAC-Item.')
+    
+    if 'proj:centroid' in stac_item_dict['properties']: 
+        for key in stac_item_dict['properties']['proj:centroid']: 
+            stac_item_dict['properties']['proj:centroid'][key] = float(stac_item_dict['properties']['proj:centroid'][key])
+
+    stac_item_object_final = pystac.Item.from_dict(stac_item_dict)
+    return stac_item_object_final
---
+++ lib/lib/datasets/sentinel.py
@@ -0,0 +1,376 @@
+import os
+import re
+import json
+import copy
+from datetime import datetime
+import xml.etree.ElementTree as ET
+import logging
+import pystac
+import fnmatch
+
+from ..base.file import calculate_checksum
+from ..resources.stac import (
+    extract_stactools,
+    add_asset_filesize,
+)
+
+scene_id_patterns = {
+    "S1":
+        r"^(?P<sensor>S1[AB])_"
+        r"(?P<beam>S1|S2|S3|S4|S5|S6|IW|EW|WV|EN|N1|N2|N3|N4|N5|N6|IM)_"
+    
+        r"(?P<product>SLC|GRD|OCN)"
+        r"(?P<resolution>F|H|M|_)_"
+        r"(?P<processingLevel>1|2)"
+        r"(?P<category>S|A)"
+        r"(?P<pols>SH|SV|DH|DV|VV|HH|HV|VH)_"
+        r"(?P<start>[0-9]{8}T[0-9]{6})_"
+        r"(?P<stop>[0-9]{8}T[0-9]{6})_"
+        r"(?P<orbitNumber>[0-9]{6})_"
+        r"(?P<dataTakeID>[0-9A-F]{6})_"
+        r"(?P<productIdentifier>[0-9A-F]{4})$",
+    "S2":
+        r"^(?P<sensor>S2[AB])_"
+        r"MSI"
+        r"(?P<level>L1C|L2A)_"
+        r"(?P<start>[0-9]{8}T[0-9]{6})_"
+        r"(?P<processingBaseline>N[0-9]{4})_"
+        r"R(?P<orbitNumber>[0-9]{3})_"
+        r"T(?P<utm_zone>[0-9]{2})"
+        r"(?P<mgrs_lat>[A-Z]{1})"
+        r"(?P<square>[A-Z]{2})_"
+        r"(?P<productDiscriminator>[0-9]{8}T[0-9]{6})$",
+    "S3":
+        r"^(?P<sensor>S3[AB])_"
+        r"(?P<instrument>OL|SL|SR|DO|MW|GN|SY|TM|AX)_"
+        r"(?P<processingLevel>0|1|2)_"
+        r"(?P<product>[A-Z0-9_]{6})_"
+        r"(?P<start>[0-9]{8}T[0-9]{6})_"
+        r"(?P<stop>[0-9]{8}T[0-9]{6})_"
+        r"(?P<productDiscriminator>[0-9]{8}T[0-9]{6})_"
+        r"(?P<instance>[A-Z0-9_]{17})_"
+        r"(?P<center>[A-Z0-9_]{3})_"
+        r"(?P<class>[A-Z0-9_]{8})$",
+    "S5":
+        r"^(?P<sensor>S5P)_"
+        r"(?P<fileclass>[A-Z]{4})_"
+        r"(?P<category>[A-Z0-9_]{3})_"
+        r"(?P<product>[A-Z0-9_]{6})_"
+        r"(?P<start>[0-9]{8}T[0-9]{6})_"
+        r"(?P<stop>[0-9]{8}T[0-9]{6})_"
+        r"(?P<orbitNumber>[0-9]{5})_"
+        r"(?P<collection>[0-9]{2})_"
+        r"(?P<processorVersion>[0-9]{6})_"
+        r"(?P<productionDate>[0-9]{8}T[0-9]{6})$",
+    "S5P_AUX":
+        r"^(?P<sensor>S5P)_"
+        r"(?P<fileclass>[A-Z]{4})_"
+        r"(?P<category>[A-Z0-9_]{3})_"
+        r"(?P<product>[A-Z0-9_]{6})_"
+        r"(?P<start>[0-9]{8}T[0-9]{6})_"
+        r"(?P<stop>[0-9]{8}T[0-9]{6})_"
+        r"(?P<productionDate>[0-9]{8}T[0-9]{6})$"
+}
+
+folder_structures = {
+    "S1": "{product}/{year}/{month}/{day}",
+    "S2": "{level}/tiles/{utm_zone}/{mgrs_lat}/{square}/{year}/{month}",
+    "S3": "{instrument}/{product}/{year}/{month}/{day}",
+    "S5": "{category}/{product}/{year}/{month}/{day}"
+}
+
+
+checksum_settings = {
+    "S1": dict(file="manifest.safe", algorithm="MD5"),
+    "S2": dict(file="manifest.safe", algorithm="SHA3-256"),
+    "S3": dict(file="xfdumanifest.xml", algorithm="MD5")
+}
+
+asset_changes = {
+    "S2": {
+        'coastal': 'B01',
+        'blue': 'B02',
+        'green': 'B03',
+        'red': 'B04',
+        'rededge1': 'B05',
+        'rededge2': 'B06',
+        'rededge3': 'B07',
+        'nir': 'B08',
+        'nir08': 'B8A',
+        'nir09': 'B09',
+        'cirrus': 'B10',
+        'swir16': 'B11',
+        'swir22': 'B12',
+        'visual': 'TCI',
+        'aot_10m': 'AOT',
+        'wvp_10m': 'WVP',
+        'scl': 'SCL'
+    }
+}
+
+variable_mappings = {
+    "S3": {
+        "OL": "OLCI",
+        "SL": "SLSTR",
+        "SY": "SYNERGY"
+    }
+}
+
+def get_scene_id_info(scene_id):
+    satellite = scene_id[0:2]
+    if satellite == 'S5' and '_AUX_' in scene_id:
+        satellite = 'S5P_AUX'
+    if satellite not in scene_id_patterns:
+        raise Exception("Satellite %s not supported" % satellite)
+
+    match = re.match(re.compile(scene_id_patterns[satellite]), scene_id)
+    variables = match.groupdict()
+    if 'category' in variables:
+        variables['category'] = variables['category'].rstrip('_')
+    if 'product' in variables:
+        variables['product'] = variables['product'].rstrip('_').replace('___', '_').replace('__', '_')
+    return variables
+
+
+def get_scene_id_folder(scene_id, folder_format=None):
+    variables = get_scene_id_info(scene_id)
+    if 'start' in variables:
+        date = datetime.strptime(variables['start'], '%Y%m%dT%H%M%S')
+        variables['year'] = date.strftime('%Y')
+        variables['month'] = date.strftime('%m')
+        variables['day'] = date.strftime('%d')
+    
+    if scene_id.startswith('S3'):
+        if 'instrument' in variables:
+            variables['instrument'] = variable_mappings["S3"][variables['instrument']]
+        if 'product' in variables:
+            variables['product'] = variables['instrument'][0:2] + '_' + variables['processingLevel'] + '_' + variables['product'] + '___'
+
+    if folder_format is None:
+        satellite = scene_id[0:2]
+        if satellite not in folder_structures:
+            raise Exception("Satellite %s not supported" % satellite)
+        folder_format = folder_structures[satellite]
+
+    return folder_format.format(**variables)
+
+
+def validate_integrity(scene_path, scene_id):
+    if scene_id[0:2] not in checksum_settings:
+        raise Exception("%s not supported for integrity check (%s)" % (scene_id[0:2], scene_id))
+    settings = checksum_settings[scene_id[0:2]]
+    checksum_alg = settings["algorithm"]
+    index_file_name = settings["file"]
+
+    manifest = os.path.join(scene_path, index_file_name)
+    if not os.path.exists(manifest):
+        raise Exception("manifest.safe not found: %s" % manifest)
+    
+    tree = ET.parse(manifest)
+    root = tree.getroot()
+    for f in root.findall('.//byteStream'):
+        href = f.find('fileLocation').attrib['href']
+        check_file = os.path.join(scene_path, href)
+        size = os.stat(check_file).st_size
+        expected_file_size = int(f.attrib['size'])
+        if size != expected_file_size:
+            raise Exception("File %s does not match expected file size: %s vs. %s" % (check_file, size, expected_file_size))
+        
+        expected_checksum = f.find('checksum').text.lower()
+        # checksum_alg = "SHA3-256"
+        checksum = calculate_checksum(checksum_alg, check_file)
+        if checksum != expected_checksum:
+            raise Exception("File %s does not match expected checksum (%s): %s vs. %s" % (check_file, checksum_alg, size, expected_checksum))
+    
+    return True
+
+
+def sentinel_metadata(scene_path, scene_id, return_pystac=False, add_file_size=False):
+    if scene_path[-1] == "/":
+        scene_path = scene_path[:-1]
+    print("executing sentinel_metadata for %s" % scene_path)
+    
+    if not os.path.exists(scene_path):
+        raise Exception("metadata_error: Folder does not exist %s" % (scene_path))
+    
+    stac_function = None
+    stac_function_args = {}
+    if scene_id.startswith('S1') and '_GRD' in scene_id:
+        stac_function = 'stactools.sentinel1.stac.create_item'
+    elif scene_id.startswith('S1') and '_SLC' in scene_id:
+        stac_function = 'stactools.sentinel1.stac.create_item'
+    elif scene_id.startswith('S2'):
+        stac_function = 'stactools.sentinel2.stac.create_item'
+    elif scene_id.startswith('S3'):
+        stac_function = 'stactools.sentinel3.stac.create_item'
+        stac_function_args = dict(skip_nc=True)
+    else:
+        raise Exception("metadata_error: No STAC function for %s" % scene_id)
+    
+    base_item = None
+    collection = get_collection_name(scene_id)
+    if collection == 'sentinel-2-c1-l2a':
+        base_item = os.path.join(os.path.dirname(__file__), 'collections', 'sentinel', 'sentinel-2-c1-l2a.json')
+    elif collection == 'sentinel-2-c1-l1c':
+        base_item = os.path.join(os.path.dirname(__file__), 'collections', 'sentinel', 'sentinel-2-c1-l1c.json')
+    elif collection == 'sentinel-3-olci-l1-efr':
+        base_item = os.path.join(os.path.dirname(__file__), 'collections', 'sentinel', 'sentinel-3-olci-l1-efr.json')
+
+    try:
+        stac_file = os.path.join(scene_path, scene_id + ".STAC.json")
+        stac_item = extract_stactools(scene_path, stac_function, stac_function_args)
+        stac_item.properties['terrabyte:stactools_id'] = stac_item.id
+        stac_item.id = scene_id
+
+        if base_item:
+            base_item = json.load(open(base_item))
+            if 'item_assets' in base_item:
+                base_item['assets'] = base_item['item_assets']
+
+        if scene_id.startswith('S2'):
+            stac_item = modify_s2_stac(stac_item, base_item=base_item)
+        elif scene_id.startswith('S3'):
+            stac_item = modify_s3_stac(stac_item, base_item=base_item)
+
+        
+        # Add file:// protocol for local file paths
+        for asset in stac_item.assets: 
+            if stac_item.assets[asset].href.startswith('/'):
+                stac_item.assets[asset].href = 'file://%s' % stac_item.assets[asset].href
+
+        if add_file_size:
+            stac_item = add_asset_filesize(stac_item)
+
+        if return_pystac:
+            return stac_item
+        else:
+            with open(stac_file, 'w') as f:
+                f.write(json.dumps(stac_item.to_dict()))
+            return stac_file
+    except Exception as e:
+        metadata_error = "Error during creating metadata for %s: %s" % (
+            scene_path,
+            str(e),
+        )
+        raise Exception(
+            "metadata_error: %s" % metadata_error
+        )
+
+
+def get_collection_name(scene_id):
+    if scene_id.startswith('S1') and '_GRD' in scene_id:
+        return 'sentinel-1-grd'
+    elif scene_id.startswith('S1') and '_SLC' in scene_id:
+        return 'sentinel-1-slc'
+    elif scene_id.startswith('S2') and '_MSIL1C_' in scene_id:
+        return 'sentinel-2-c1-l1c'
+    elif scene_id.startswith('S2') and '_MSIL2A_' in scene_id:
+        return 'sentinel-2-c1-l2a'
+    elif scene_id.startswith('S3') and '_OL_1_EFR_' in scene_id:
+        return 'sentinel-3-olci-l1-efr'
+    elif scene_id.startswith('S5') and '_L1B_' in scene_id:
+        return 'sentinel-5p-l1b'
+    elif scene_id.startswith('S5') and '_L2_' in scene_id:
+        return 'sentinel-5p-l2'
+    elif scene_id.startswith('S5') and '_AUX_' in scene_id:
+        return 'sentinel-5p-aux'
+    else:
+        raise Exception("No collection found")
+
+
+
+log = logging.getLogger("Log Info")
+def modify_s2_stac(stac_item: pystac.item.Item, base_item=None):
+    """Modify the Asset-Keys and eo:bands:name for a Sentinel-2 L2 STAC-Item.
+    
+    Args:
+        stac_item: The STAC item file/object to modify. Must be a STACObject.
+        
+        Returns: A pystac.item.Item object with the desired changes."""
+
+    stac_item_dict = copy.deepcopy(stac_item.to_dict(include_self_link=False))
+    
+    mission = stac_item.id[0:2]   # Get first two characters of Item id (e.g., S2 for S2A_MSIL2A_....)
+    if mission not in asset_changes:
+        raise Exception("Could not find entry for %s in asset_changes configuration" % mission)
+    input_dict = asset_changes[mission]
+    assets_new = dict()
+
+    for i, (current_key, target_key) in enumerate(input_dict.items()):
+
+        if current_key in stac_item_dict["assets"]:
+            log.info(f'Replacing the current Asset-Key {current_key} with the new Asset-Key {target_key}.')
+            assets_new[target_key] = copy.deepcopy(stac_item_dict["assets"].pop(current_key))
+        else:
+            log.info(f'Replacing the current Asset-Key {current_key} with the new Asset-Key {target_key}: Current key not found in metadata!')
+
+    if base_item: 
+        try:
+            for key in base_item['assets']:
+                if key not in assets_new:
+                    assets_new[key] = copy.deepcopy(stac_item_dict['assets'][key])
+                for property in base_item['assets'][key]:
+                    assets_new[key][property] = copy.deepcopy(base_item['assets'][key][property])
+
+
+        except Exception as e:
+            log.error('ERROR: %s' % e)
+
+    stac_item_dict['assets'] = assets_new
+    stac_item_object_final = pystac.Item.from_dict(stac_item_dict)
+    return stac_item_object_final
+
+
+
+def modify_s3_stac(stac_item: pystac.item.Item, base_item=None):
+    """Modify the Asset-Keys and eo:bands:name for a Sentinel-3 STAC-Item.
+    
+    Args:
+        stac_item: The STAC item file/object to modify. Must be a STACObject.
+        
+        Returns: A pystac.item.Item object with the desired changes."""
+
+    stac_item_dict = copy.deepcopy(stac_item.to_dict(include_self_link=False))
+    
+    assets_new = dict()
+    for key in stac_item_dict['assets']:
+        asset = copy.deepcopy(stac_item_dict['assets'][key])
+        for property in stac_item_dict['assets'][key]:
+            if property.startswith('file:'):
+                del asset[property]
+        
+        assets_new[key] = asset
+
+    if base_item: 
+        try:
+            for key in base_item['assets']:
+                if key not in assets_new:
+                    assets_new[key] = copy.deepcopy(stac_item_dict['assets'][key])
+                for property in base_item['assets'][key]:
+                    assets_new[key][property] = copy.deepcopy(base_item['assets'][key][property])
+                if 'resolution' in assets_new[key]:
+                    del assets_new[key]['resolution']
+    
+        except Exception as e:
+            log.error('ERROR: %s' % e)
+    
+    stac_item_dict['assets'] = assets_new
+
+    info = get_scene_id_info(stac_item.id)
+    tby_item_id = f"{info['sensor']}_{info['instrument']}_{info['processingLevel']}_{info['product']}_{info['start']}_{info['stop']}_{info['instance']}"
+    stac_item_dict['properties']['terrabyte:uniq_id'] = tby_item_id
+
+    if 's3:productType' in stac_item_dict['properties']:
+        stac_item_dict['properties']['s3:product_type'] = stac_item_dict['properties']['s3:productType']
+        del stac_item_dict['properties']['s3:productType']
+
+    info = stac_item_dict['id'].split('_')
+    timeliness = info[-2]
+    baseline_collection = info[-1]
+    stac_item_dict['properties']['s3:processing_timeliness'] = timeliness
+    stac_item_dict['properties']['s3:baseline_collection'] = baseline_collection
+    stac_item_object_final = pystac.Item.from_dict(stac_item_dict)
+    return stac_item_object_final
+
+
+
